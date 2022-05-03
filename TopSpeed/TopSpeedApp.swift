9ce5c537105599989fa2d484b518e35f6c1cf61a
Cooper Standard <cooper@cooperstandard.org>
2022-05-02 18:35:59 -0600

    Initial Commit

---
+++ TopSpeed/TopSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopSpeedApp.swift
+//  TopSpeed
+//
+//  Created by Cooper Standard on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TopSpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
